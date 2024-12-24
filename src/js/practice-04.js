8b37e0aacc507e5bc4f20cccf0f437f8c4fcb734
Artem Matvieientsev <tmatveentsev@gmail.com>
2024-12-24 19:49:09 +0100

    method, tasks
    map, flatMap, filter, find, findIndex,  some, every, reduce, tasks

--- src/js/practice-04.js
+++ src/js/practice-04.js
@@ -178,3 +178,160 @@ function handleSuccess(message) {
 function handleError(productId) {
   console.log(`Error! product with id:${productId} not found`);
 }
+
+
+
+const users = [
+  { id: 1, name: 'John', age: 28, skills: ['JavaScript', 'HTML', 'CSS'] },
+  { id: 2, name: 'Alice', age: 32, skills: ['Python', 'Data Analysis'] },
+  { id: 3, name: 'Bob', age: 24, skills: ['JavaScript', 'React', 'Node.js'] },
+  { id: 4, name: 'Emily', age: 40, skills: ['Java', 'Spring'] },
+  { id: 5, name: 'Davis', age: 22, skills: ['C++', 'C#'] },
+];
+
+// method map метод перебираючий масив, повертає новий масив такоїж довжини. Для його роботи потрібна тільки callback function
+
+const names = users.map(({ name }, idx, arr) => name); // працює з деструктуризацією. Idx та arr не обовʼязкові
+console.log(names);
+
+function customMap(arr) {
+  const result = []
+  for (let i = 0; i < arr.length; i += 1) {
+    result.push(arr[i].name)
+  }
+  return result;
+};
+
+console.log(customMap(users));
+
+
+// method flatMap метод який дозволяє увійти на одну вкладеність та отримати данні, повертає так само масив данних
+
+const skills = users.flatMap(({ skills }) => skills) // є також method flat(deep)
+console.log(skills);
+
+// фільтрація повторення за довомогою indexOf на результаті виконання flatMap
+
+const uniqueSkills = skills.filter((skill, idx, arr) => {
+  return arr.indexOf(skill) === idx
+});
+console.log(uniqueSkills);
+
+
+// method filter (часто використовуємий метод), сортування за умовою, повертає обʼєкт який задовільняє умову
+
+const ageUser = users.filter(({ age }, idx, arr) => age > 30).map(({ name }) => name); // лише перший параметр обовʼязковий 
+console.log(ageUser);
+
+
+// method find метод який шукає перший елемент який задовільняє умову
+
+const searchSkill = users.find(({ skills }) => skills.includes('JavaScript'));
+console.log(searchSkill);
+
+
+// method findIndex подібний метод до indexOf але працює не з примітивами а з складним  типом данних, перериває пошук після першого елементу який задовільняє умову або повертає -1
+
+const index = users.findIndex(({ id }) => id === 3); // можна знайти індекс елементу який треба видалити 
+console.log(index);
+
+// some працює подібно до методу includes, але вміє працювати з складним типом данних повертає true або false
+
+const isKnow = users.some(({ skills }, idx, arr) => skills.includes('Python'));
+console.log(isKnow);
+
+
+// every перевіряє щоб усі елементи ітеруємого обʼєкта задовільнив умову, повертае true or false
+
+const oldAge = users.every(({ age }) => age > 20 && age < 50);
+console.log(oldAge);
+
+
+// method sort метод який мутує початковий масив 
+
+const sortUsers = [...users].sort((a, b) => a.age - b.age) // a - поточний елемент ітерації, b - натсупний елемент ітерації // за допомогою spred можна не мутуючи оригінал використати метод на копії
+console.log(users);
+console.log(sortUsers);
+
+const sortName = [...users].sort((a, b) => a.name.localeCompare(b.name)) // сортування за допомогою localCompare рядків за алфавітним порядком
+console.log(sortName);
+
+
+// method reduce універсальний метод 
+
+const totalAge = users.reduce((acc, { age }, idx, arr) => {  // перші два параметри обовʼязкові
+  acc += age
+  return acc
+}, 0); 
+console.log(totalAge);
+
+const allName = users.reduce((acc, { name }) => {
+  acc.push(name);
+  return acc
+}, [])
+console.log(allName);
+
+
+// Tasks
+
+//Task 1
+// Напишіть функцію, яка використовує метод mар, щоб створити новий масив обʼєктів, в якому буде - інформація про середній бал кожного студента.
+
+const students = [
+  {name:  'John', grades: [80, 85, 90] },
+  {name:  'Alice', grades: [90, 95, 92] },
+  {name: 'Bob', grades:  [70, 80, 75] },
+  { name: 'Emily', grades: [95, 92, 88] },
+  { name: 'David', grades: [85, 88, 90] }
+];
+
+function getAverage(arr) {
+  return arr.map(({ name, grades }) => {
+    const total = grades.reduce((acc, item) => acc + item, 0)
+    const obj = {
+      name,
+      averge: Math.round(total / grades.length),
+    };
+    return obj
+  })
+}
+console.log(getAverage(students));
+
+// Task 2
+// Напишіть функцію, яка використовує метод filter, щоб створити новий масив, в якому будуть тільки студенти які старше 20 років
+
+const students2= [
+  { name: 'John', age: 20, gpa: 3.8 },
+  { name: 'Alice', age: 21, gpa: 3.2 },
+  { name: 'Bob', age: 19, gpa: 3.5 },
+  { name: 'Emily', age: 22, gpa: 3.9 },
+  { name: 'David', age: 20, gpa: 3.7 }
+];
+
+function getAdult(arr) {
+  return arr.filter(({ age }) => age > 20);
+}
+console.log(getAdult(students2));
+
+// Task 3
+// Напишіть функцію, яка - використовує метод find, щоб знайти книжку за її назвою (title)
+
+const books = [
+  { title: 'JavaScript:  The Good Parts', author: 'Douglas Crockford', year: 2008 },
+  { title: 'Clean Code:  A Handbook of Agile Software Craftsmanship', author: 'Robert C. Martin', year: 2008 },
+  { title: 'The Pragmatic Programmer:  Your Journey to Mastery', author: 'Andrew Hunt, David Thomas' , year: 1999 },
+  { title: 'Design Patterns: Elements of Reusable Object-Oriented Software', author:  'Erich Gamma, RichardHelm, Ralph Johnson, John Vlissides', year: 1994 },
+  { title: 'Refactoring: Improving the Design of Existing Code', author: 'Martin Fowler', year: 1999 }];
+
+function getBook(arr, title) {
+  return arr.find(({title : bookTitle}) => bookTitle === title) || 'Not found'
+}
+console.log(
+  getBook(
+    books,
+    "Design Patterns: Elements of Reusable Object-Oriented Software"
+  )
+);
+
+// Task 4
+//
